79bad2a65b1d3515a08e1635c33cae6f14187851
John Joseph <atiquajohn@gmail.com>
2017-06-18 22:46:18 +0000

    connet with the DB

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes    =require("./routes/comments"),
     campgroundRoutes =require("./routes/campgrounds"),  
     indexRoutes      =require("./routes/index");  
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://john:john@ds041140.mlab.com:41140/yelpcamp");
 app.use(bodyParser.urlencoded({extended:true}));    
 app.set("view engine" , "ejs");
 app.use(express.static(__dirname+"/public"));
